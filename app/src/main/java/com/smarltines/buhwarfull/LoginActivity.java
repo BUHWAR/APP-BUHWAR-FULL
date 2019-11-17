e567b5362364f8f5c6f534dbefddaa8dbd7b6bcc
jesusrv1103 <jesus21c@hotmail.com>
2019-11-17 15:55:37 -0600

    necesario

--- app/src/main/java/com/smarltines/buhwarfull/LoginActivity.java
+++ app/src/main/java/com/smarltines/buhwarfull/LoginActivity.java
@@ -1,14 +1,14 @@
 package com.smarltines.buhwarfull;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import com.smarltines.buhwarfull.colon.activity.MainActivity;
-import com.smarltines.buhwarfull.guard.GuardMainActivity;
+        import com.smarltines.buhwarfull.colon.activity.MainActivity;
+        import com.smarltines.buhwarfull.guard.GuardMainActivity;
 
 public class LoginActivity extends AppCompatActivity {
     private Button btnLogin;
@@ -22,7 +22,7 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                Intent intent=new Intent(LoginActivity.this, MainActivity.class);
+                Intent intent=new Intent(LoginActivity.this, GuardMainActivity.class);
                 startActivity(intent);
             }
         });
