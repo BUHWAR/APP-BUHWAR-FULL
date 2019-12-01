0cd24910470657dc13f41e17374349d8b2ba0266
Alejandro Gonzalez <inge.alejandro.gonzalez@gmail.com>
2019-11-30 22:49:15 -0600

    Vistas Update

--- app/src/main/java/com/smartlines/buhwarfull/guard/ui/visitor/VisitorFragment.java
+++ app/src/main/java/com/smartlines/buhwarfull/guard/ui/visitor/VisitorFragment.java
@@ -1,13 +1,18 @@
 package com.smartlines.buhwarfull.guard.ui.visitor;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -58,6 +63,29 @@ public class VisitorFragment extends Fragment {
         String strDate = "" + dateFormat.format(calender.getTime());
         txtDateVistante.setText(strDate);
         txtTimeVisitante.setText(strTime);
+
+
+
+        txtAdressVisitado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                final String[] listItems = {"one", "two", "three", "four", "five"};
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Choose item");
+
+                builder.setItems(listItems, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        txtAdressVisitado.setText( listItems[which]);
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
 
         String[] opcSospecha = {"Bajo", "Moderado", "Alto"};
         ArrayAdapter adapterPlace = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, opcSospecha);
