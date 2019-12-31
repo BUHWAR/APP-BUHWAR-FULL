2fc3926bcb459e20e187cab9d55f0cbf4af7bcdc
Alejandro Gonzalez <inge.alejandro.gonzalez@gmail.com>
2019-12-30 18:23:21 -0600

    INSERT firestore test

--- app/src/main/java/com/smartlines/buhwarfull/guard/ui/incidents/IncidentsFragment.java
+++ app/src/main/java/com/smartlines/buhwarfull/guard/ui/incidents/IncidentsFragment.java
@@ -1,40 +1,55 @@
 package com.smartlines.buhwarfull.guard.ui.incidents;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
 
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.smartlines.buhwarfull.R;
+
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class IncidentsFragment extends Fragment {
 
-    private IncidentsViewModel sendViewModel;
     private TextView txtDateInc;
     private TextView txtTimeInc;
+    private TextView txtName;
     private Spinner spinnerPlace;
     private Spinner spinnerViews;
+   private FirebaseFirestore firebaseDatabase;
+    private Button btnSend;
+    private  Map<String, Object> incidents;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        sendViewModel =
-                ViewModelProviders.of(this).get(IncidentsViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_incidents, container, false);
         txtTimeInc = root.findViewById(R.id.txtTimeInc);
         txtDateInc = root.findViewById(R.id.txtDateInc);
         spinnerPlace = root.findViewById(R.id.spinnerPlace);
         spinnerViews = root.findViewById(R.id.spinnerViews);
+        txtName = root.findViewById(R.id.txtGuardName);
+        btnSend = root.findViewById(R.id.btnSendInc);
+        firebaseDatabase = FirebaseFirestore.getInstance();
+
         Calendar calender = Calendar.getInstance();
         SimpleDateFormat mdformat = new SimpleDateFormat("HH:mm:ss");
         String strTime = "Hora actual : " + mdformat.format(calender.getTime());
@@ -53,6 +68,38 @@ public class IncidentsFragment extends Fragment {
         txtTimeInc.setText(strTime);
         txtDateInc.setText(strDate);
 
+
+
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                incidents = new HashMap<>();
+                incidents.put("time", txtTimeInc.getText());
+                incidents.put("date", txtDateInc.getText());
+                incidents.put("observations", spinnerViews.getSelectedItem().toString());
+                incidents.put("place", spinnerPlace.getSelectedItem().toString());
+                incidents.put("nameGuard", "Test");
+
+             firebaseDatabase.collection("incidents")
+                        .add(incidents)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.v("IncidentsFragment", "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.v("IncidentsFragment", "Error adding document", e);
+                            }
+                        });
+
+            }
+        });
         return root;
     }
+
+
 }
